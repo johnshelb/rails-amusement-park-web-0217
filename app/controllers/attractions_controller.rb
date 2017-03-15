e01861e19586f51d371d9c65da69ecbbb1b6a507
johnshelb <jsnd11@gmail.com>
2017-03-15 07:34:37 -0400

    fixed business emails

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,13 @@
+class AttractionsController < ApplicationController
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find_by(id:params[:id])
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+end
